2ff0985e6327986f0255fb7d45adc75b7eedfd1c
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-05-19 10:57:26 +0200

    Clean up DB on startup

--- exporter_test.sh
+++ exporter_test.sh
@@ -14,6 +14,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+# clean up database
+psql postgresql://postgres:postgres@localhost:5432/test?sslmode=disable --file setup/clean_database.sql
 
 export PATH=$PATH:/tmp/ramdisk/
 export NOVENV=1
