ccd2f8821794c1d8122274bbd162935ca46eb6a3
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-19 08:45:29 +0200

    Docstring

--- mocks/insights-content-service/content_server.py
+++ mocks/insights-content-service/content_server.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Mock server that can be used instead of fully functional Content Service."""
+
 import json
 from fastapi import FastAPI, Response
 
