e00f93f9c5b1713badcdfefd37d8677aca790bae
Pavel Tisnovsky <ptisnovs@redhat.com>
2022-10-19 08:47:52 +0200

    Fixed docstring

--- mocks/token-refreshment/token_refreshment.py
+++ mocks/token-refreshment/token_refreshment.py
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+"""Mock service that can be used to simulate access token gathering."""
+
 from fastapi import FastAPI, Form
 from starlette.responses import JSONResponse
 from pydantic import BaseModel
