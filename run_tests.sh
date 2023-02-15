9087d9828bcb6541d4676f1dc02de4eb0b232dea
Papa Bakary Camara <pcamara@redhat.com>
2023-02-15 14:55:19 +0100

    Fix run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -1,12 +1,20 @@
 #!/bin/bash -ex
 
-export NOVENV=1
-
 function prepare_venv() {
-    virtualenv -p python3 venv && source venv/bin/activate && python3 `which pip3` install -r requirements.txt
+    virtualenv -p python3 venv && source venv/bin/activate
+    python3 "$(which pip3)" install -r requirements.txt
+    for f in requirements/*.txt; do
+        echo "File -> $f" && python3 "$(which pip3)" install -r "$f" || exit 1
+    done
 }
 
-[ "$NOVENV" == "1" ] || prepare_venv || exit 1
+# prepare virtual environment if necessary
+case "$NOVENV" in
+    "0") echo "using existing virtual env";;
+    "1") prepare_venv;;
+esac
 
-PYTHONDONTWRITEBYTECODE=1 python3 "`which behave`" --tags=-skip -D dump_errors=true @feature_list.txt $@
+for f in test_list/*.txt; do
+    PYTHONDONTWRITEBYTECODE=1 python3 -m behave --tags=-skip -D dump_errors=true @"$f" "$@" || { echo "Tests in $f failed."; }
+done
 
