9c23783f6eaba21ed539ebd5cff2b1eca2f6eda1
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-01 08:47:27 +0100

    Test step to read current migration number from database

---
+++ features/steps/aggregator_storage.py
@@ -0,0 +1,35 @@
+# Copyright © 2023 Pavel Tisnovsky, Red Hat, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Insights results aggregator database-related operations performed by BDD tests."""
+
+from behave import then, when
+import psycopg2
+
+
+MIGRATION_INFO_TABLE = "migration_info"
+
+
+@when("I read current migration number from database")
+def read_migration_number_from_database(context):
+    """Test step to read current migration number from database."""
+    cursor = context.connection.cursor()
+    try:
+        cursor.execute(f"SELECT version from {MIGRATION_INFO_TABLE} limit 1")
+        context.database_migration = cursor.fetchone()[0]
+        assert isinstance(context.database_migration, int)
+        context.connection.commit()
+    except Exception as e:
+        context.connection.rollback()
+        raise e
