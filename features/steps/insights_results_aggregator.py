dd9034582bdfb1e8ad37312b8f7b2b558f000959
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-03-29 08:08:35 +0200

    Test step to check returned rule hits for given cluster

--- features/steps/insights_results_aggregator.py
+++ features/steps/insights_results_aggregator.py
@@ -418,3 +418,33 @@ def check_rule_hits(context, expected_count=1, cluster="11111111-2222-3333-4444-
     # compare actual count with expected count
     assert actual_count == expected_count, \
         f"Expected rule hits count: {expected_count}, actual count: {actual_count}"
+
+
+@then("I should find following rule hits in returned cluster report for cluster {cluster}")
+def check_returned_cluster_report_list(context, cluster):
+    """Check reported rules against table with expected rule results."""
+    reports = retrieve_reports(context, cluster)
+
+    # check if all rule hits defined in scenario is found in returned structure
+    for rule_hit in context.table:
+        expected_type = rule_hit["Type"]
+        expected_rule_id = rule_hit["Rule ID"]
+        expected_error_key = rule_hit["Error key"]
+
+        # construct full rule ID together with error key
+        expected_rule_id += "|" + expected_error_key
+
+        # try to find the corresponding record in rule hits returned by service
+        for record in reports:
+            actual_type = record["details"]["type"]
+            actual_rule_id = record["rule_id"]
+            actual_error_key = record["details"]["error_key"]
+
+            # exact match
+            if all((actual_type == expected_type,
+                    actual_rule_id == expected_rule_id,
+                    actual_error_key == expected_error_key)):
+                break
+        else:
+            # record was not found
+            raise KeyError(f"Rule hit {rule_hit} was not returned by the service")
