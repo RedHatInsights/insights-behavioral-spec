689fad414222365eca4419b4b18c89c90daf3a0e
Papa Bakary Camara <pcamara@redhat.com>
2023-04-03 12:15:10 +0200

    Content service endpoints test - fix BuildTime check

--- features/steps/insights_content_service.py
+++ features/steps/insights_content_service.py
@@ -31,7 +31,7 @@ def check_build_time(context):
     """Check build timestamp taken from service output."""
     buildTime = context.response.json()["info"]["BuildTime"]
     pattern = re.compile(
-        r'.{3} .{3}[ ]{1,2}[0-9]* [0-9]{2}:[0-9]{2}:[0-9]{2} [AP]M [A-Z]{3} [0-9]{4}')
+        r'.{3} .{3}[ ]{1,2}[0-9]{1,2} [0-9]{2}:[0-9]{2}:[0-9]{2} ([AP]M )?[A-Z]{1,5} [0-9]{4}')
     match = re.match(pattern, buildTime)
     assert match.group(0), "BuildTime is not a date time: {}".format(buildTime)
 
