7fa6a37c156903fd537c570b2189f0d500976f54
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-04-11 09:15:58 +0200

    Merge code coverage reports after tests in CCX Notification Writer

--- notification_writer_tests.sh
+++ notification_writer_tests.sh
@@ -85,3 +85,8 @@ fi
 
 PYTHONDONTWRITEBYTECODE=1 python3 -m behave --tags=-skip -D dump_errors=true @test_list/notification_writer.txt "$@"
 
+
+if [[ "${flag}" == "coverage" ]]
+then
+    code_coverage_report
+fi
