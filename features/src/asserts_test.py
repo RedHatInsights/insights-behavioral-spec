d81379f4d1c33854d4659ab57d31729e7ac57900
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-10 11:43:08 +0200

    Make linter happy

--- features/src/asserts_test.py
+++ features/src/asserts_test.py
@@ -9,4 +9,3 @@ def test_asserts_sets_equality():
     assert_sets_equality("something", {1}, {1})
     assert_sets_equality("something", {1, 2}, {1, 2})
     assert_sets_equality("something", {1, 2}, {2, 1})
-
