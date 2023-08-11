50303ab1d5bb8e61eaa5ec0004fa96d306fe4345
Pavel Tisnovsky <ptisnovs@redhat.com>
2023-08-11 09:06:05 +0200

    Unit test stub

---
+++ features/src/utils_test.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+# vim: set fileencoding=utf-8
+
+# Copyright © 2023  Pavel Tisnovsky
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+"""Unit tests for functions defined in utils.py source file."""
+
+import pytest
+from utils import get_array_from_json, construct_rh_token, retrieve_set_of_clusters_from_table
+
+
