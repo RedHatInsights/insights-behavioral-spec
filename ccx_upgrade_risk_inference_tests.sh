edc0d5c4a7e789e6c73894ed7285c3fa9feb2cca
Papa Bakary Camara <pcamara@redhat.com>
2023-09-25 15:44:57 +0200

    Fix shell syntax issues

--- ccx_upgrade_risk_inference_tests.sh
+++ ccx_upgrade_risk_inference_tests.sh
@@ -34,8 +34,8 @@ function prepare_venv() {
 }
 
 function install_inference_service() {
-    python3 "$(which pip3)" install -r "$PATH_TO_LOCAL_INFERENCE_SERVICE"/requirements.txt || exit 1
-    python3 "$(which pip3)" install "$PATH_TO_LOCAL_INFERENCE_SERVICE"/. || exit 1
+    python3 "$(which pip3)" install -r "$PATH_TO_LOCAL_INFERENCE_SERVICE"/requirements.txt || exit 1
+    python3 "$(which pip3)" install "$PATH_TO_LOCAL_INFERENCE_SERVICE"/. || exit 1
     # shellcheck disable=SC2016
     add_exit_trap 'python3 "$(which pip3)" uninstall -y ccx-upgrades-inference'
 }
